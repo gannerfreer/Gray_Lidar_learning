08f440d8277e9c93a186ffc09e3829ef0c570ac9
Gray <g-ray@qq.com>
2023-12-11 16:19:35 +0800

    禁止test_preintegration文件的编译以解决gmock无法编译的问题

--- src/gray/run_gins_gray.cc
+++ src/gray/run_gins_gray.cc
@@ -2,7 +2,7 @@
  * @Author: Gray g-ray@qq.com
  * @Date: 2023-12-11 15:49:08
  * @LastEditors: Gray g-ray@qq.com
- * @LastEditTime: 2023-12-11 15:54:24
+ * @LastEditTime: 2023-12-11 16:13:25
  * @FilePath: /Gray_Lidar_learning/src/gray/run_gins_gray.cc
  * @Description: 
  */
@@ -15,13 +15,15 @@
 #include <fstream>
 #include <iomanip>
 
-
+using namespace sad;
 
 int main(int argc, char** argv) {
     google::InitGoogleLogging(argv[0]);  // 初始化log当前目录
     FLAGS_stderrthreshold = google::INFO;//log等级为INFO级别
     FLAGS_colorlogtostderr = true;       // 设置记录到标准输出的颜色消息（如果终端支持）
     google::ParseCommandLineFlags(&argc, &argv, true);
+
+
 
     return 0;
 }
